64eaab0e3848d449e6a1e974ceb8bf9b5b3f4906
Mutiulrasool <mutiulrasool769@gmail.com>
2024-12-25 23:52:19 +0400

    completed the section2

--- layout.js
+++ layout.js
@@ -22,30 +22,6 @@ var showTheNavItemsOnClick = () => {
 };
 
 
-
-
-// var showTheNavItemsOnClick = () => {
-
-//     let navBarListItems = document.getElementsByClassName("nav-link");
-//     for (let i = 0; i < navBarListItems.length; i++) {
-//         navBarListItems[i].classList.add("fade"); // Apply fade initially
-//         setTimeout(() => {
-//             navBarListItems[i].classList.add("show"); // Trigger fade-in after delay
-//         }, 1000); // Increased delay
-//     }
-//     // let navBarList = document.getElementById("navToShowOnclick");
-//     // navBarList.style.display = "flex";
-
-//     let alertBox = document.getElementsByClassName("alert")[0];
-//     if (alertBox) {
-//         alertBox.style.display = 'none';
-//     }
-// }
-
-// document.getElementsByClassName("closeAlert&showNavItems")[0].addEventListener("click", showTheNavItemsOnClick);
-// document.getElementsByClassName("closeAlert&showNavItems")[1].addEventListener("click", showTheNavItemsOnClick);
-
-
 document.getElementsByClassName("closeAlert&showNavItems")[0].addEventListener("click", showTheNavItemsOnClick);
 document.getElementsByClassName("closeAlert&showNavItems")[1].addEventListener("click", showTheNavItemsOnClick);
 
